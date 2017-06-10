d4bbd840b187d574a118d6c65b44310c60a9d24e
G53120 <noam.shaish@nordea.com>
2017-06-11 00:44:57 +0200

    Initial integration test commit

---
+++ ci-example-integration-tests/src/test/scala/ci/example/BarIntegration.scala
@@ -0,0 +1,12 @@
+package ci.example
+
+import org.scalatest.{FlatSpec, Matchers}
+
+/**
+  * Created by G53120 on 6/11/2017.
+  */
+class BarIntegration extends FlatSpec with Matchers{
+  behavior of "Bar application"
+
+
+}
