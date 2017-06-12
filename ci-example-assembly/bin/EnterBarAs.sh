57a389b47b1ce47312819d9449d852c541a4cd1c
G53120 <noam.shaish@nordea.com>
2017-06-13 01:24:45 +0200

    Added CI external version

--- ci-example-assembly/bin/EnterBarAs.sh
+++ ci-example-assembly/bin/EnterBarAs.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd $(dirname "$0")
-java -cp ci-example-1.0.0-1-SNAPSHOT-jar-with-dependencies.jar ci.example.Bar $1
+java -cp ${project.parent.artifactId}-${project.version}-jar-with-dependencies.jar ci.example.Bar $1
